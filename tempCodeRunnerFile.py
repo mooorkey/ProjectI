ec54eb9828876786cd0dbdf0d11b916bca4b9f2a
mooorkey <i.morkey@msn.com>
2024-04-08 09:12:58 +0700

    Added Test

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,2 +1,6 @@
-for worker in worker_datas:
-        print(worker)
+    # print(result)
+    for worker in result:
+        if worker[1] != -1:
+            print(f"worker {worker[0]} to jobID {worker[1]['JobId']} rank {worker[2]}")
+        else:
+            print(f"worker {worker[0]} to jobID same rank {worker[2]}")
